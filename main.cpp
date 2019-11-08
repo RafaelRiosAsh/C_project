01961ed46b68d58613b1d9490335c9726b2f5b01
Rafael Rios <Wukengch@gmail.com>
2019-11-08 00:15:52 -0600

    added small input function to simplify code

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 /**
  * C++ beginner program designed to learn about the project testing object oriented programming
  * @author Rafael Rios
- * @version 1.5 since 1.0
+ * @version 2.0 since 1.0
  */
 //function to simplify printing to console to save time
 template < typename T > void print(T t){
@@ -34,33 +34,32 @@ static bool contains(std::string list, char character){
     return false;
 }
 
+//fun test function to make C++ look more like Java or python
+template < typename T > auto input (T t){
+    std::string ans;
+    printl(t);
+    std::cin >> ans;
+    return ans;
+}
+
 int main() {
-    int age;
+    std::string age;
     std::string name;
     std::string lastName;
 
     printl("please enter your name");
-    std::cin>> name;
-    std::cin>> lastName;
-
-    printl("please enter your age");
-    std::cin>> age;
-
-
+    std::cin >> name;
+    std::cin >> lastName;
 
     printl("your name is ",name," your last name is ",lastName," your age is ",age);
 
     //random test function that checks if a character is inside a string
     bool hasWord = contains("Frogissima", 'a');
-
     if (hasWord){
         printl("the word contains the character");
     }
     else {
         printl("the word does not contain the character");
     }
-
-
-
     return 0;
 }
